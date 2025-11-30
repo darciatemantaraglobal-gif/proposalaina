e820632ad0b3f731a9842e0c20437133e663a2d8
darciatemantaraglobal-gif <darciatemantaraglobal@gmail.com>
2025-11-30 18:13:06 +0700

    Update HeroSection.jsx

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
         {/* Logo AINA */}
         <div className="flex items-center gap-3 relative z-50">
           <img
-            src="/AIGHYPT-MEYUALA.png"
+            src="/AIGHYPT MEYUALA.png"
             alt="AINA Logo"
             className="h-10 w-auto object-contain hover:scale-105 transition-transform duration-300"
           />
